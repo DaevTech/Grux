303d8339f83d51b15e25addbc62ebcc41a1d71b7
Brian Søgård Jensen <bsj@succesprojekter.dk>
2025-11-24 12:38:42 +0100

    Handle errors when creating logs and passing it up the chain. It will still panic, but that is fine if logs are not writable

--- src/grux_log.rs
+++ src/grux_log.rs
@@ -1,3 +1,4 @@
+use crate::core::operation_mode::OperationMode;
 use log::{LevelFilter, SetLoggerError, trace};
 use log4rs::{
     append::{
@@ -9,12 +10,10 @@ use log4rs::{
     filter::threshold::ThresholdFilter,
 };
 use std::fs::OpenOptions;
-use crate::core::operation_mode::OperationMode;
 
 // Initilize the logging
 pub fn init_logging(operation_mode: OperationMode) -> Result<log4rs::Handle, String> {
     let log_path = get_log_location().map_err(|e| format!("Failed to get log location: {}", e))?;
-    let file_path = format!("{}/system.log", &log_path);
 
     // Build a stderr logger.
     let stderr = ConsoleAppender::builder()
@@ -23,55 +22,55 @@ pub fn init_logging(operation_mode: OperationMode) -> Result<log4rs::Handle, Str
         .build();
 
     // Logging to log file.
+    let system_log_file_path = format!("{}/system.log", &log_path);
     let logfile = FileAppender::builder()
         // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/indeonex.html
         .encoder(Box::new(PatternEncoder::new("{h({d(%d-%m-%Y %H:%M:%S%.3f)(utc)} - {l}: {m}{n})}")))
-        .build(&file_path)
-        .unwrap();
+        .build(&system_log_file_path)
+        .map_err(|e| format!("Failed to create system log file '{}/system.log': {}", log_path, e))?;
 
     // Log Trace level output to file where trace is the default level
     // and the programmatically specified level to stderr.
     let trace_logfile = FileAppender::builder()
         .encoder(Box::new(PatternEncoder::new("{h({d(%d-%m-%Y %H:%M:%S%.3f)(utc)} - {l}: {m}{n})}")))
-        .build(format!("{}/trace.log", get_log_location().map_err(|e| format!("Failed to get log location: {}", e))?))
-        .unwrap();
+        .build(format!("{}/trace.log", &log_path))
+        .map_err(|e| format!("Failed to create trace log file '{}/trace.log': {}", log_path, e))?;
 
-    let config;
-
-    match operation_mode {
+    // Build the log4rs config based on operation mode
+    let config = match operation_mode {
         OperationMode::DEV => {
             // We truncate the log files on each start in dev mode
             let _ = OpenOptions::new().write(true).truncate(true).create(true).open(format!("{}/system.log", &log_path)).unwrap();
             let _ = OpenOptions::new().write(true).truncate(true).create(true).open(format!("{}/trace.log", &log_path)).unwrap();
 
-            config = Config::builder()
+            Config::builder()
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Info))).build("logfile", Box::new(logfile)))
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Info))).build("stderr", Box::new(stderr)))
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Trace))).build("trace", Box::new(trace_logfile)))
                 .build(Root::builder().appender("logfile").appender("stderr").appender("trace").build(LevelFilter::Trace))
-                .unwrap();
+                .map_err(| e | format!("Failed to build log4rs config for DEV mode: {}", e))?
         }
         OperationMode::DEBUG => {
-            config = Config::builder()
+            Config::builder()
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Debug))).build("logfile", Box::new(logfile)))
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Info))).build("stdout", Box::new(stderr)))
                 .build(Root::builder().appender("logfile").appender("stdout").build(LevelFilter::Debug))
-                .unwrap();
+                .map_err(| e | format!("Failed to build log4rs config for DEBUG mode: {}", e))?
         }
         OperationMode::PRODUCTION => {
-            config = Config::builder()
+            Config::builder()
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Info))).build("logfile", Box::new(logfile)))
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Info))).build("stderr", Box::new(stderr)))
                 .build(Root::builder().appender("logfile").appender("stderr").build(LevelFilter::Info))
-                .unwrap();
+                .map_err(| e | format!("Failed to build log4rs config for PRODUCTION mode: {}", e))?
         }
         OperationMode::SPEEDTEST => {
-            config = Config::builder()
+            Config::builder()
                 .appender(Appender::builder().filter(Box::new(ThresholdFilter::new(LevelFilter::Error))).build("stderr", Box::new(stderr)))
                 .build(Root::builder().appender("stderr").build(LevelFilter::Error))
-                .unwrap();
+                .map_err(| e | format!("Failed to build log4rs config for SPEEDTEST mode: {}", e))?
         }
-    }
+    };
 
     // Use this to change log levels at runtime.
     // This means you can change the default log level to trace
